388bdfa14e67cf1b31c2840f03d2ea09a21f9d94
Sandeep Valapi <sandeepvalapi@gmail.com>
2017-08-10 05:11:10 +0530

    Code clean

--- src/main/java/com/findbugs/examples/OfConcernRankBugs.java
+++ src/main/java/com/findbugs/examples/OfConcernRankBugs.java
@@ -1,30 +1,17 @@
 package com.findbugs.examples;
 
 public class OfConcernRankBugs {
-	/** The place holder. */
+
 	private String placeHolder = null;
 
-	/**
-	 * The Constructor.
-	 */
 	public OfConcernRankBugs() {
 		this.placeHolder = "Test String";
 	}
 
-	/**
-	 * Gets the place holder.
-	 *
-	 * @return the place holder
-	 */
 	private String getPlaceHolder() {
 		return this.placeHolder;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
 	@Override
 	public boolean equals(Object obj) {
 		OfConcernRankBugs object = (OfConcernRankBugs) obj;
